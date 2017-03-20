959cba03be1d2c4012105791f0876291afb43e1e
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-03-20 14:41:19 -0700

    created the Tasks controller and created the route to index

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,5 @@
+class TasksController < ApplicationController
+  def index
+    # @task
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+
+  # this create a rout to view 
+  get 'tasks', to: 'tasks#index'
+
+  # get 'tasks/:id', to: 'tasks#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
