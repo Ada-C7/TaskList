67759a1c086288b1402734674cbfeaef606f6f07
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-03-20 16:44:37 -0700

    added show functionality for individual task, added prev and next task links to show pages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,27 @@
 class TasksController < ApplicationController
 
-  attr_reader :tasks
   def index
     @tasks = [
-      {id: 1, title: "Sweep the ceiling", duedate: "01-01-2020", priority: "HIGH"},
-      {id: 2, title: "Iron the pancakes", duedate: "12-31-3036", priority: "LOW"},
-      {id: 3, title: "Mushroom the fungi", duedate: "09-21-1908", priority: "MEDIUM"}
+      {id: "1", title: "Sweep the ceiling", duedate: "01-01-2020", priority: "HIGH"},
+      {id: "2", title: "Iron the pancakes", duedate: "12-31-3036", priority: "LOW"},
+      {id: "3", title: "Mushroom the fungi", duedate: "09-21-1908", priority: "MEDIUM"}
     ]
   end
 
   def show
-    @task = params[:id]
+    tasks = [
+      {id: "1", title: "Sweep the ceiling", duedate: "01-01-2020", priority: "HIGH"},
+      {id: "2", title: "Iron the pancakes", duedate: "12-31-3036", priority: "LOW"},
+      {id: "3", title: "Mushroom the fungi", duedate: "09-21-1908", priority: "MEDIUM"}
+    ]
+    @result_task = nil
+    tasks.each do |task|
+      if params[:id] == task[:id]
+        @result_task = task
+      end
+    end
+
+    @prev_id = (params[:id].to_i - 1).to_s
+    @next_id = (params[:id].to_i + 1).to_s
   end
 end
