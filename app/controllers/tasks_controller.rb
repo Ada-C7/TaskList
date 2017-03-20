a1da9a76c9803ef85635700259f7a9c1d9c7d697
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-03-20 16:38:16 -0700

    working on style of pages but nothing fancy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApplicationController
         { name: "Go to Brunch", description: "Location - Portage Bay" },
         { name: "Go to Lunch", description: "Location: Thai", completed_at: 'random_time' },
         { name: "Go to Second Lunch", description: "Location: Cafe Ladro" },
-        { name: "Play Video Games", description: "Zelda", completed_at: 'random_time' },
+        { name: "Play Video Games", description: "Zelda Ocarina of Time", completed_at: 'random_time' },
         { name: "High Five Somebody You Don't Know", description: "", completed_at: 'random_time' },
         { name: "Plant Flowers", description: "sun flowers, roses, lavander", completed_at: 'random_time' },
         { name: "Call Mom", description: "at 6:30pm" },
