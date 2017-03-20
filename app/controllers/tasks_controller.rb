9d927adc221063c2c91b896e3a042eda015d3a56
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-03-20 14:50:26 -0700

    displaying hard-coded tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,9 @@
 class TasksController < ApplicationController
   def index
+    @tasks = [
+      {title: "Sweep the ceiling", duedate: "01-01-2020", priority: "HIGH"},
+      {title: "Iron the pancakes", duedate: "12-31-3036", priority: "LOW"},
+      {title: "Mushroom the fungi", duedate: "09-21-1908", priority: "MEDIUM"}
+    ]
   end
 end
