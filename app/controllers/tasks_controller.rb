2f3864d0538a5cce802e8e8b39093e12fa16ad86
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-03-20 14:47:53 -0700

    created aa controller action for task index -an array of hard-coded tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,10 @@
 class TasksController < ApplicationController
   def index
-    # @task
+    @tasks = [
+              { task: "grocery shop", complete_by: "date" },
+              { task: "clean bedroom", complete_by: "date" },
+              { task: "homework", complete_by: "date" },
+              { task: "workout-gym", complete_by: "date" }
+             ]
   end
 end
