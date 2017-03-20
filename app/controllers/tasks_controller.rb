9b5bf8d2a39db27f806e803203cbc260ddcb8d45
Sahana Murthy <s.murthy730.com>
2017-03-20 14:43:18 -0700

    Created @tasks hash

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,7 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = [{ task: "Pet a puppy", priority: "High"},
+              { task: "Buy groceries", priority: "Low"},
+              { task: "Clean room", priority: "High"}]
+  end
 end
