47f7afac4e0c0b925488b7dfad39e54592fca264
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-03-20 15:30:17 -0700

    added show to controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,15 @@
 class TasksController < ApplicationController
+
+  attr_reader :tasks
   def index
     @tasks = [
-      {title: "Sweep the ceiling", duedate: "01-01-2020", priority: "HIGH"},
-      {title: "Iron the pancakes", duedate: "12-31-3036", priority: "LOW"},
-      {title: "Mushroom the fungi", duedate: "09-21-1908", priority: "MEDIUM"}
+      {id: 1, title: "Sweep the ceiling", duedate: "01-01-2020", priority: "HIGH"},
+      {id: 2, title: "Iron the pancakes", duedate: "12-31-3036", priority: "LOW"},
+      {id: 3, title: "Mushroom the fungi", duedate: "09-21-1908", priority: "MEDIUM"}
     ]
+  end
+
+  def show
+    @task = params[:id]
   end
 end
