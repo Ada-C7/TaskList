3978c30faf5c75dceeda7b8cac712613f604544f
Sahana Murthy <s.murthy730.com>
2017-03-20 14:56:58 -0700

    Basic tasks list showing on screen

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,19 @@
 class TasksController < ApplicationController
   def index
-    @tasks = [{ task: "Pet a puppy", priority: "High"},
-              { task: "Buy groceries", priority: "Low"},
-              { task: "Clean room", priority: "High"}]
+    @tasks = [
+  { name: "The First Task", description: "", completed_at: "random_time" },
+  { name: "Go to Brunch", description: "" },
+  { name: "Go to Lunch", description: "", completed_at: "random_time" },
+  { name: "Go to Second Lunch", description: "" },
+  { name: "Play Video Games", description: "", completed_at: "random_time" },
+  { name: "High Five Somebody You Don't Know", description: "", completed_at: "random_time" },
+  { name: "Plant Flowers", description: "", completed_at: "random_time" },
+  { name: "Call Mom", description: "" },
+  { name: "She worries, you know.", description: "" },
+  { name: "Nap.", description: "", completed_at: "random_time" },
+  { name: "Pet a puppy", description: "", completed_at: "random_time" }]
+  end
+
+  def show
   end
 end
