367dc3a068122dafed4323562190d393c032f789
jjousun <jjousun@gmail.com>
2017-03-20 15:08:00 -0700

    Created ERB viewer

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,10 @@
 class TasksController < ApplicationController
   def index
-    @tasks = [sleep, code, eat, laugh]
+    @tasks = [
+      { task: "sleep", priority: "high" },
+      { task: "code", priority: "high" },
+      { task: "eat", priority: "medium" },
+      { task: "buy groceries", priority: "medium" }
+    ]
   end
 end
