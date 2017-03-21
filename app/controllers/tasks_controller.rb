a6f93a0d41823ad77e935788e3ae42359b3b62d8
Ann Dai <gofarann@gmail.com>
2017-03-21 14:57:10 -0700

    added database schema for task, changed view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,15 @@
 class TasksController < ApplicationController
   def index
-    @tasks = ["homework", "laundry", "groceries", "insurance"]
+    @tasks = Task.all
+    #   { "completed": ["laundry", "grocery shopping", "bills" ] },
+    #   { "in progress": ["homework", "pack bags", "taxes"] },
+    #   { "uninitiated": ["insurance"] }
+    # ]
   end
+
+  def show
+    @task_result = Task.find params[:id]
+
+  end
+
 end
