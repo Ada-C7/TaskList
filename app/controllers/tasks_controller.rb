c3f8f1f758c66356bfd00880154ce4b6951798f0
Theresa Manney <theresa.m.17@gmail.com>
2017-03-20 17:26:28 -0700

    Added dynamic images and dynamic pages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,9 @@
 class TasksController < ApplicationController
   def index
     @tasks = [
-      "Laundry",
-      "Groceries",
-      "Walk dog",
-      "Dance",
-      "Work-out" 
+      {short_term: ["task1", "task2", "task3"]},
+      {medium_term: ["task1", "task2", "task3"]},
+      {big_goals: ["task1", "task2", "task3"]}
     ]
   end
 end
