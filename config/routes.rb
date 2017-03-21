b929a0318848df9aad269aeb5441acdefee5b6b3
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-03-21 12:26:51 -0700

    added some notes to the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # this create a rout to view
   get 'tasks', to: 'tasks#index'
 
+  # the as task is a little bit of "rails magic" that sets up routes to each individual task
   get 'tasks/:id', to: 'tasks#show', as: 'task'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
