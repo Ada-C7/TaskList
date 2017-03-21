ac971194a538a438796c5870268abe984ad53e63
Tamiko Terada <tamikodevo@gmail.com>
2017-03-21 11:47:23 -0700

    created database schema for Tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,21 @@
 class TasksController < ApplicationController
+
+  def index
+    puts ">>>TT: from inside TasksController"
+    @tasks = [
+      { name: "The First Task", description: "", priority: "low" },
+      { name: "Go to Brunch", description: "with some friends" },
+      { name: "Go to Lunch", description: "", priority: "normal"  },
+      { name: "Go to Second Lunch", description: "ugh" },
+      { name: "Play Video Games", description: "Pokemon GO!", priority: "low" },
+      { name: "High Five Somebody You Don't Know", description: "", priority: "" },
+      { name: "Plant Flowers", description: "", priority: "low" },
+      { name: "Call Mom", description: "" },
+      { name: "She worries, you know.", description: "" },
+      { name: "Nap.", description: "at least 2 hours", priority: "high"  }
+    ]
+  end
+
+  def show
+  end
 end
