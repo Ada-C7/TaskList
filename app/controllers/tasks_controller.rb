9e2ce749be29cc186a67f7ccedf7b2ac65005415
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-03-21 16:24:13 -0700

    changes made through Wave 1

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,30 @@
+class TasksController < ApplicationController
+
+# TASKS = [
+#   { name: "The First Task", description: "Wake up. Turn off alarm."  },
+#   { name: "Go to Brunch", description: "Meet Jenni & crew at Wayward." },
+#   { name: "Go to Lunch", description: "1pm. meet mentor" },
+#   { name: "Go to Second Lunch", description: "3pm. Meet sisters." },
+#   { name: "Play Video Games", description: "Ratchet & Clank" },
+#   { name: "High Five Somebody You Don't Know", description: "pick random stranger on the street." },
+#   { name: "Plant Flowers", description: "stop at nursery on the way home." },
+#   { name: "Call Mom", description: "at 3p. Try land line first" },
+#   { name: "She worries, you know.", description: "Try and draw her about concerns." },
+#   { name: "Nap.", description: " 3 hours at a minimum"}
+# ]
+
+  # def random_time
+  #     Time.at(rand * Time.now.to_i)
+  # end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    id = params[:id].to_i
+    @task = Task.find(id)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index', as: 'welcome'
+
+  get 'tasks', to: 'tasks#index', as: 'tasks'
+
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
