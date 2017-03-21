1d2a3754c31cea49c72eceb58d241ceca45474f2
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-03-20 17:01:20 -0700

    class exercises

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  get '/tasks', to: 'tasks#index'
+  # NEED comma after identifier 'tasks'
+
+  get '/tasks/:id', to: 'tasks#show', as: 'task' 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
