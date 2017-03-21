92e41a1e7edf65eeded1d37cb4f6f7405ee79dba
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-03-21 10:10:14 -0700

    fixed prev and next buttons so won't break if you try to go beyond list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,8 +20,24 @@ class TasksController < ApplicationController
         @result_task = task
       end
     end
+    ids = []
+    tasks.each do |task|
+      ids << task[:id].to_i
+    end
 
-    @prev_id = (params[:id].to_i - 1).to_s
-    @next_id = (params[:id].to_i + 1).to_s
+    max_id = ids.max
+    min_id = ids.min
+
+    if (params[:id].to_i - 1) >= min_id
+      @prev_id = (params[:id].to_i - 1).to_s
+    else
+      @prev_id = params[:id]
+    end
+
+    if (params[:id].to_i + 1) <= max_id
+      @next_id = (params[:id].to_i + 1).to_s
+    else
+      @next_id = params[:id]
+    end
   end
 end
