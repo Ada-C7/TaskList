9115821a70fdf9ed4325c034ede2671374f14257
Regan Huff <regan.huff@gmail.com>
2017-03-21 14:18:54 -0700

    Populate db and adjust index.html and styles

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
   def index
 
-    @tasks = [
-      "order pens",
-      "stop by book launch",
-      "print outline for talk",
-      "practice talk"
-    ]
+    # @tasks = [
+    #   "order pens",
+    #   "stop by book launch",
+    #   "print outline for talk",
+    #   "practice talk"
+    # ]
+    @tasks = Task.all
   end
 end
