090414c9c55238a027d08c493bb5eab2c188b33d
Tehut Getahun <tehut.m.g@gmail.com>
2017-03-21 10:26:52 -0700

    generate model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-
-
-  get '/tasks', to:'tasks#index'
-
-
+  get '/tasks', to:'tasks#index', as:'tasks'
   get '/tasks/:id', to:'tasks#show', as:'task'
+  get 'welcome', to:'welcome#index', as:'welcome'
 
 end
