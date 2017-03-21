5e407329be2e1c55cbf0ad3eb1f30ea74f552311
Tehut Getahun <tehut.m.g@gmail.com>
2017-03-21 09:11:20 -0700

    changes to route and task_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,26 @@
 class TasksController < ApplicationController
+  TASKS =[
+    { task: "walk to school", deadline: "8:30 am, M-F" , id:1},
+    { task: "pray", deadline: "7:30 am, M-Su", id:2},
+    { task: "physical therapy", deadline: "9:30 pm, M-F", id:3},
+    { task: "homework", deadline: "6:30 pm, M-F", id:4}
+  ]
+
   def index
     # puts "Inside the puts in books#index!"
-    @tasks = [{ task: "walk to school", deadline: "8:30 am, M-F"},
-              { task: "pray", deadline: "7:30 am, M-Su"},
-              { task: "physical therapy", deadline: "9:30 pm, M-F"},
-              { task: "homework", deadline: "6:30 pm, M-F"}]
+    @tasks = TASKS
+  end
+
+
+  def show
+    id = params[:id].to_i
+  @task = TASKS[id]
+    # @id =[]
+    # @tasks.each do |task|
+    #   if task[:id] == params[:id]
+    #     @id << "Task ##{task[:id]}: #{task[:task]} deadline:#{task[:deadline]}"
+    #   end
+    #   @id
+    # end
   end
 end
