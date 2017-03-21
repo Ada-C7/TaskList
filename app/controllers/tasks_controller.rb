a2aa4f88876d1bc5971ffe691fe4ee23d977fd92
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-03-21 13:54:56 -0700

    created the model tatask and migrated the db

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,23 @@
 class TasksController < ApplicationController
 
-  TASKS = [
-        { name: "The First Task", description: "Be amazing", completed_at: 'random_time' },
-        { name: "Go to Brunch", description: "Location - Portage Bay" },
-        { name: "Go to Lunch", description: "Location: Thai", completed_at: 'random_time' },
-        { name: "Go to Second Lunch", description: "Location: Cafe Ladro" },
-        { name: "Play Video Games", description: "Zelda Ocarina of Time", completed_at: 'random_time' },
-        { name: "High Five Somebody You Don't Know", description: "", completed_at: 'random_time' },
-        { name: "Plant Flowers", description: "sun flowers, roses, lavander", completed_at: 'random_time' },
-        { name: "Call Mom", description: "at 6:30pm" },
-        { name: "Nap", description: "45 min max", completed_at: 'random_time' }
-      ]
+  # TASKS = [
+  #       { name: "The First Task", description: "Be amazing", completed_at: 'random_time' },
+  #       { name: "Go to Brunch", description: "Location - Portage Bay" },
+  #       { name: "Go to Lunch", description: "Location: Thai", completed_at: 'random_time' },
+  #       { name: "Go to Second Lunch", description: "Location: Cafe Ladro" },
+  #       { name: "Play Video Games", description: "Zelda Ocarina of Time", completed_at: 'random_time' },
+  #       { name: "High Five Somebody You Don't Know", description: "", completed_at: 'random_time' },
+  #       { name: "Plant Flowers", description: "sun flowers, roses, lavander", completed_at: 'random_time' },
+  #       { name: "Call Mom", description: "at 6:30pm" },
+  #       { name: "Nap", description: "45 min max", completed_at: 'random_time' }
+  #     ]
 
   def index
-    @tasks = TASKS
+    # @tasks = TASKS
   end
 
   def show
-    id = params[:id].to_i
-    @task = TASKS[id]
+    # id = params[:id].to_i
+    # @task = TASKS[id]
   end
 end
