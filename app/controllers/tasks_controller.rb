9d4fe64f7ead14d3c361a9cea69a81859d41a5a8
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-03-21 16:01:18 -0700

    updated show and index to use new model task and the data I added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,25 +1,16 @@
 class TasksController < ApplicationController
 
   def index
-    @tasks = [
-      {id: "1", title: "Sweep the ceiling", duedate: "01-01-2020", priority: "HIGH"},
-      {id: "2", title: "Iron the pancakes", duedate: "12-31-3036", priority: "LOW"},
-      {id: "3", title: "Mushroom the fungi", duedate: "09-21-1908", priority: "MEDIUM"}
-    ]
+
+    @tasks = Task.all
+
   end
 
   def show
-    tasks = [
-      {id: "1", title: "Sweep the ceiling", duedate: "01-01-2020", priority: "HIGH"},
-      {id: "2", title: "Iron the pancakes", duedate: "12-31-3036", priority: "LOW"},
-      {id: "3", title: "Mushroom the fungi", duedate: "09-21-1908", priority: "MEDIUM"}
-    ]
-    @result_task = nil
-    tasks.each do |task|
-      if params[:id] == task[:id]
-        @result_task = task
-      end
-    end
+
+    @result_task = Task.find(params[:id])
+    tasks = Task.all
+
     ids = []
     tasks.each do |task|
       ids << task[:id].to_i
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  get 'days/index'
+
+  get 'days/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+
   get "/tasks", to: "tasks#index"
-  get "/tasks/:id", to: "tasks#show"
+  get "/tasks/:id", to: "tasks#show", as: "task"
+
 end
