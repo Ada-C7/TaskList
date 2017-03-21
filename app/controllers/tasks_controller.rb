a706f346b7f34ef2042c544b43e3e6bdd041b871
Tamiko Terada <tamikodevo@gmail.com>
2017-03-21 14:41:53 -0700

    git push to resolve issues for rails console

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,24 @@
 class TasksController < ApplicationController
 
   def index
-    puts ">>>TT: from inside TasksController"
-    @tasks = [
-      { name: "The First Task", description: "", priority: "low" },
-      { name: "Go to Brunch", description: "with some friends" },
-      { name: "Go to Lunch", description: "", priority: "normal"  },
-      { name: "Go to Second Lunch", description: "ugh" },
-      { name: "Play Video Games", description: "Pokemon GO!", priority: "low" },
-      { name: "High Five Somebody You Don't Know", description: "", priority: "" },
-      { name: "Plant Flowers", description: "", priority: "low" },
-      { name: "Call Mom", description: "" },
-      { name: "She worries, you know.", description: "" },
-      { name: "Nap.", description: "at least 2 hours", priority: "high"  }
-    ]
+    puts '>>>TT: from TasksController#index'
   end
 
   def show
+    puts '>>>TT: from inside TasksController#show'
   end
 end
+
+
+# @tasks = [
+#   { name: "The First Task", description: "", priority: "low" },
+#   { name: "Go to Brunch", description: "with some friends" },
+#   { name: "Go to Lunch", description: "", priority: "normal"  },
+#   { name: "Go to Second Lunch", description: "ugh" },
+#   { name: "Play Video Games", description: "Pokemon GO!", priority: "low" },
+#   { name: "High Five Somebody You Don't Know", description: "", priority: "" },
+#   { name: "Plant Flowers", description: "", priority: "low" },
+#   { name: "Call Mom", description: "" },
+#   { name: "She worries, you know.", description: "" },
+#   { name: "Nap.", description: "at least 2 hours", priority: "high"  }
+# ]
--- db/migrate/20170321173539_create_tasks.rb
+++ db/migrate/20170321173539_create_tasks.rb
@@ -1,9 +1,9 @@
-class CreateTasks < ActiveRecord::Migration[5.0]  
+class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
       t.string :name
       t.string :description
-      t.date :completion_date
+      t.datetime :completion_date
 
       t.timestamps
     end
