175962c42cd2fa602da704237c5cfacc4241f90b
Regan Huff <regan.huff@gmail.com>
2017-03-22 13:22:17 -0700

    Add show, new and create methods and views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,22 @@
 class TasksController < ApplicationController
   def index
-
-    # @tasks = [
-    #   "order pens",
-    #   "stop by book launch",
-    #   "print outline for talk",
-    #   "practice talk"
-    # ]
     @tasks = Task.all
+  end
+
+  def show
+    @result_task = Task.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    task = Task.new
+    task.name = params[:name]
+    task.description = params[:description]
+    task.context = params[:context]
+    if task.save
+      redirect_to tasks_path
+    end
   end
 end
