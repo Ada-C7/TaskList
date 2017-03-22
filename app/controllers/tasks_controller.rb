7ba5653ab10995743b374afa386e03d15dd6206d
natalia-ku <bilberryfm@mail.ru>
2017-03-22 15:24:54 -0700

    added edit and add new from

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,27 +5,36 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
   end
 
   def create
-  puts ">>>> NK: In Tasks controller create method"
-  puts params[:task].to_hash
+    puts ">>>> NK: In Tasks controller #create method"
+    puts params[:task].to_hash
 
-  Task.create(task_params)
-  redirect_to tasks_path
-end
-
-  def show
-    id = params[:id].to_i
-    @task = Task.find(id)
+    Task.create(task_params)
+    redirect_to tasks_path
   end
 
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update_attributes(task_params)
+    task.save
+
+    redirect_to task_path(task)
+  end
 
   private
-
   def task_params
     return params.require(:task).permit(:name, :description, :completed_at,:location)
   end
-
 
 end
