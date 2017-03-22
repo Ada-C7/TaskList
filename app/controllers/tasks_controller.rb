cb2404c9779aa004abb649a3de02b342b7a618d4
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-03-22 13:40:04 -0700

    have function to add new task working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,9 +9,15 @@ class TasksController < ApplicationController
   end
 
   def create
+    # puts ">>>>>>>>>>>>>>>>#{params[:complete_by]}>>>>>>>>>>>>>>"
+    # Time.parse(params[:complete_by])
+      # date.strftime("'%Y-%m-%d'");
+    # params[:complete_by] = DateTime.parse(params[:complete_by])
+    # params[:complete_by]
     # puts ">>>> CDC: In Tasks Controller#create"
     # puts params[:task].to_hash
     Task.create(task_params)
+    redirect_to tasks_path
   end
 
   def show
