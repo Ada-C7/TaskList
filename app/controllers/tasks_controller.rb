3a096623911ecf2da9328e6be5c7ff850242ad0f
Anacelia Gillette <acgillette426@gmail.com>
2017-03-22 16:31:56 -0700

    Finished Wave 2, started Sass styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,30 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def show
+
+    @result_task = Task.find(params[:id])
+  end
+
+  def create
+    task = Task.new
+    task.title = params[:task][:title]
+    task.description = params[:task][:description]
+    task.completion = "Not Yet Completed"
+    if task.save
+      redirect_to tasks_path
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :completion)
+  end
+
+
 end
