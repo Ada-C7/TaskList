600a387f99e638c187274784e56390101d94ebca
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-03-22 14:35:34 -0700

    Finished with wave 1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,20 @@ class TasksController < ApplicationController
   def show
     @show_task = Task.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    task = Task.new
+    task.name = params[:name]
+    task.description = params[:description]
+    task.completion_date = params[:completion_date]
+    task.img_url = params[:img_url]
+
+
+    if task.save
+      redirect_to tasks_path
+    end
+  end
 end
