a2f5f0df31b68b30dcec132fa63317d9c2e45706
laura <lmelgarejos@unal.edu.co>
2017-03-22 16:22:02 -0700

    Wave 3 finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,22 +12,26 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
-  def edit
-    task = Task.find(params[:id])
-    task(task_params)
-    task.save
-
-    redirect_to tasks_path(task.id)
-  end
-
   def create
     Task.create(task_params)
+
     redirect_to tasks_path
   end
 
   def show
-    id = params[:id].to_i
-    @task = Task.find(id)
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update_attributes(task_params)
+    task.save
+
+    redirect_to tasks_path(task)
   end
 
   private
