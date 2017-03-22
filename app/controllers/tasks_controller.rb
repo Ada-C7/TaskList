1be897199c6bb1cec368e5cd239c6faa854a4008
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-03-22 11:14:34 -0700

    have new form working but need to add completed_by param

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,11 +5,24 @@ class TasksController < ApplicationController
   end
 
   def new
+    # Task.new
+  end
 
+  def create
+    # puts ">>>> CDC: In Tasks Controller#create"
+    # puts params[:task].to_hash
+    Task.create(task_params)
   end
 
   def show
     id = params[:id].to_i
     @task = Task.find(id)
   end
+
+  #helper method to verify the given info from the user
+private
+  def task_params
+    return params.require(:task).permit(:name, :description, :complete_by)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # root 'tasks#index', as: 'tasks'
-  # this create a rout to view
+  # this sets the home page
 
   # put routes that act on a collection first
-  get 'tasks', to: 'tasks#index'
+  get 'tasks', to: 'tasks#index', as: 'tasks'
 
   # routes that create or edit go in middle
   get 'tasks/new', to: 'tasks#new', as: 'new_task'
+
+  post 'tasks', to: 'tasks#create'
 
   # routes that act on one instance go here
   # the as task is a little bit of "rails magic" that sets up routes to each individual task
