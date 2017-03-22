6b9bbd918f80eebac3bdbb911857ac2a53abd614
laura <lmelgarejos@unal.edu.co>
2017-03-21 22:57:20 -0700

    modified MVC to work with the model fields

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,27 +3,30 @@ class TasksController < ApplicationController
   def self.random_time
     Time.at(rand * Time.now.to_i)
   end
-
-  TASKS = [
-    { name: "The First Task", description: "", completed_at: random_time },
-    { name: "Go to Brunch", description: "" },
-    { name: "Go to Lunch", description: "", completed_at: random_time },
-    { name: "Go to Second Lunch", description: "" },
-    { name: "Play Video Games", description: "", completed_at: random_time },
-    { name: "High Five Somebody You Don't Know", description: "", completed_at: random_time },
-    { name: "Plant Flowers", description: "", completed_at: random_time },
-    { name: "Call Mom", description: "" },
-    { name: "She worries, you know.", description: "" },
-    { name: "Nap.", description: "", completed_at: random_time }
-  ]
+  #
+  # TASKS = [
+  #   { name: "The First Task", description: "", completed_at: random_time },
+  #   { name: "Go to Brunch", description: "" },
+  #   { name: "Go to Lunch", description: "", completed_at: random_time },
+  #   { name: "Go to Second Lunch", description: "" },
+  #   { name: "Play Video Games", description: "", completed_at: random_time },
+  #   { name: "High Five Somebody You Don't Know", description: "", completed_at: random_time },
+  #   { name: "Plant Flowers", description: "", completed_at: random_time },
+  #   { name: "Call Mom", description: "" },
+  #   { name: "She worries, you know.", description: "" },
+  #   { name: "Nap.", description: "", completed_at: random_time }
+  # ]
 
   def index
-    @taskslist = TASKS
+    @tasks = Task.all
   end
 
   def show
+    # id = Task.find(:id).to_i
     id = params[:id].to_i
-    @task = TASKS[id]
+
+    @task = Task.find(id)
+    # @task = TASKS[id]
 
   end
 end
