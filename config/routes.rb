80d7dc57097680a3b34093ef52f3ec2e748dcbf2
Emma Claire Philippides <acme9898@gmail.com>
2017-03-21 21:33:25 -0700

    added welcome page and links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'welcome/index'
+
+  root 'welcome#index', as: 'welcome'
+
+
   get 'tasks', to: 'tasks#index'
 
   get 'tasks/:id', to: 'tasks#show'
-  # get 'tasks/:name', to: 'tasks#random_time'
+
 end
