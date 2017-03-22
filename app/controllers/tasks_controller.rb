9b794d1135212b72e482211887b0405b0c7f979b
Alix Hamilton <alix.hamilton@gmail.com>
2017-03-21 21:45:07 -0700

    adds edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,11 +12,28 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(name: params[:task][:name], description: params[:task][:description])
+    @task = Task.new(
+      name: params[:task][:name],
+      description: params[:task][:description]
+    )
+
     if @task.save
       redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(name: params[:task][:name], description: params[:task][:description])
+      redirect_to tasks_path
+    else
+      render :edit
     end
   end
 end
