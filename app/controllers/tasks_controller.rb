ec016da6a3271ea79ad2e40505231d9cde36750d
Alix Hamilton <alix.hamilton@gmail.com>
2017-03-21 21:23:30 -0700

    add show and new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,17 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description])
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
