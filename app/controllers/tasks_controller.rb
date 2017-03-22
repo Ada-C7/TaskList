3ec954142da9b0d43a8ae6a5db75e6330c5bf1ec
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-03-22 12:05:01 -0700

    I had the wrong singular/plural of words and had book instead of task in places. Submit form made

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,12 +3,21 @@ class TasksController < ApplicationController
     Time.at(rand * Time.now.to_i)
   end
 
-TASKS = [{ name: "Breakfast", description: "Don't forget to eat", completion_date: random_time },
-         { name: "Lunch", description: "Eat MOAR. Food 4 lyfe", completion_date: random_time },
-         { name: "Dinner", description: "Don't forget to eat. OMG!", completion_date: random_time }]
+# TASKS = [{ name: "Breakfast", description: "Don't forget to eat", completion_date: random_time },
+#          { name: "Lunch", description: "Eat MOAR. Food 4 lyfe", completion_date: random_time },
+#          { name: "Dinner", description: "Don't forget to eat. OMG!", completion_date: random_time }]
 
   def index
     @tasks = Task.all
+  end
+
+  def new
+
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to tasks_path
   end
 
   def show
@@ -16,4 +25,8 @@ TASKS = [{ name: "Breakfast", description: "Don't forget to eat", completion_dat
     @task = Task.find(id)
   end
 
+private
+  def task_params
+    return params.require(:task).permit(:name, :description)
+  end
 end
