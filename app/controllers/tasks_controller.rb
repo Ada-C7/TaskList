8d12d1fa95e8b3cbe72c8e36f1f2b35cabebf338
Sahana Murthy <s.murthy730.com>
2017-03-22 16:31:50 -0700

    Completed Wave 1, Wave 2, Wave 3. For those of you who are monitoring my git hygiene, I am so sorry I haven't been pushing and committing more. I didn't know that we would be submitting this assignment. I would have done a much better job of committing otherwise. I am hoping to commit at a more acceptable rate going forward. Excited for Wave 4. Need to work on CSS and webpage display.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,51 @@
 class TasksController < ApplicationController
+
+  TASKS = [
+{ name: "The First Task", description: "", completed_at: "random_time" },
+{ name: "Go to Brunch", description: "" },
+{ name: "Go to Lunch", description: "", completed_at: "random_time" },
+{ name: "Go to Second Lunch", description: "" },
+{ name: "Play Video Games", description: "", completed_at: "random_time" },
+{ name: "High Five Somebody You Don't Know", description: "", completed_at: "random_time" },
+{ name: "Plant Flowers", description: "", completed_at: "random_time" },
+{ name: "Call Mom", description: "" },
+{ name: "She worries, you know.", description: "" },
+{ name: "Nap.", description: "", completed_at: "random_time" },
+{ name: "Pet a puppy", description: "", completed_at: "random_time" }]
+
   def index
-    @tasks = [
-  { name: "The First Task", description: "", completed_at: "random_time" },
-  { name: "Go to Brunch", description: "" },
-  { name: "Go to Lunch", description: "", completed_at: "random_time" },
-  { name: "Go to Second Lunch", description: "" },
-  { name: "Play Video Games", description: "", completed_at: "random_time" },
-  { name: "High Five Somebody You Don't Know", description: "", completed_at: "random_time" },
-  { name: "Plant Flowers", description: "", completed_at: "random_time" },
-  { name: "Call Mom", description: "" },
-  { name: "She worries, you know.", description: "" },
-  { name: "Nap.", description: "", completed_at: "random_time" },
-  { name: "Pet a puppy", description: "", completed_at: "random_time" }]
+    @tasks = Task.all
+  end
+
+  def new
+  end
+
+  def create
+    task_data = task_params
+    Task.create(task_data)
+
+    redirect_to tasks_path
   end
 
   def show
+    id = params[:id].to_i
+    @task = Task.find(id)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update_attributes(task_params)
+    task.save
+
+    redirect_to task_path(task)
+  end
+
+private
+  def task_params
+    return params.require(:task).permit(:name, :description, :completed_at)
   end
 end
