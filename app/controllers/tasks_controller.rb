d8556ff6cac2b36391042492bbce29fe39a33dbc
Brenna <bcmdarroch@gmail.com>
2017-03-22 13:07:30 -0700

    added form, task#new, task#create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,18 @@ class TasksController < ApplicationController
   def show
     @this_task = Task.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    task = Task.new
+    task.name = params[:name]
+    task.desc = params[:desc]
+
+    if task.save
+      redirect_to tasks_path
+    end
+  end
+
 end
