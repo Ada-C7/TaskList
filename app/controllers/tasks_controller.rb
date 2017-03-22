fa2b56c89fdf0187a97f59c3a88d2db0ecbdc2ee
Danielle Birbal <birbalds@gmail.com>
2017-03-22 14:05:43 -0700

    Completed Wave 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,8 +3,40 @@ class TasksController < ApplicationController
         @tasks = Task.all
     end
 
+    def new
+        @task = Task.new
+     end
+
+    def create
+        new
+        if @task.update(task_params)
+            redirect_to tasks_path
+        else
+            render 'new'
+    end
+  end
+
+    def edit
+        @task = Task.find(params[:id])
+     end
+
+    def update
+        edit
+        if @task.update(task_params)
+            redirect_to task_path(@task.id)
+        else
+            render 'edit'
+        end
+    end
+
     def show
-        id = params[:id].to_i + 1
-        @task = Task.find_by(id: id)
+        id = params[:id].to_i
+        @task = Task.find(id)
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:name, :description, :completed_at)
     end
 end
--- db/migrate/20170321182051_create_tasks.rb
+++ db/migrate/20170321182051_create_tasks.rb
@@ -1,11 +1,11 @@
 class CreateTasks < ActiveRecord::Migration[5.0]
-  def change
-    create_table :tasks do |t|
-      t.string :name
-      t.string :description
-      t.time :completed_at
+    def change
+        create_table :tasks do |t|
+            t.string :name
+            t.string :description
+            t.time :completed_at
 
-      t.timestamps
+            t.timestamps
+        end
     end
-  end
 end
