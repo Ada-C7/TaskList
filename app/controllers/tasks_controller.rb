5cb24bc920b5ef748425fdc6af4a18158b46ea53
Spatterjaaay <aja.pursova@gmail.com>
2017-03-22 14:50:03 -0700

    added create task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,4 +9,15 @@ class TasksController < ApplicationController
 
   def new
   end
+
+  def create
+    task = Task.new
+    task.name = params[:name]
+    task.description = params[:description]
+    task.completion_date = params[:completion_date]
+    task.completed = params[:completed]
+      if task.save
+        redirect_to tasks_path
+      end
+  end
 end
