ab35cbb7ae37e2cf0fc91e76bd0986f8a1371256
Emma Claire Philippides <acme9898@gmail.com>
2017-03-22 15:41:27 -0700

    added methods and able to add items to list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,32 @@
 class TasksController < ApplicationController
-# TASKS = [
-#   { name: "Pack bags", completed_at: "1:00 PM" },
-#   { name: "Print out reservation confirmations", completed_at: "1:30 PM" },
-#   { name: "Check-in for flight", completed_at: "2:00 PM" },
-#   { name: "Buy sunscreen", completed_at: "2:15 PM" },
-#   { name: "Prepare things kids will need", completed_at: "3:00 PM" },
-#   { name: "Pack the chargers", completed_at: "2:30 PM" },
-#   { name: "Write important info on Fridge board", completed_at: "3:00 PM" },
-#   { name: "Get cash", completed_at: "4:00 PM" },
-#   { name: "Get books from library", completed_at: "6:00 PM"},
-#   { name: "Grocery shopping", completed_at: "7:00 PM"}
-# ]
+  # TASKS = [
+  #   { name: "Pack bags", completed_at: "1:00 PM" },
+  #   { name: "Print out reservation confirmations", completed_at: "1:30 PM" },
+  #   { name: "Check-in for flight", completed_at: "2:00 PM" },
+  #   { name: "Buy sunscreen", completed_at: "2:15 PM" },
+  #   { name: "Prepare things kids will need", completed_at: "3:00 PM" },
+  #   { name: "Pack the chargers", completed_at: "2:30 PM" },
+  #   { name: "Write important info on Fridge board", completed_at: "3:00 PM" },
+  #   { name: "Get cash", completed_at: "4:00 PM" },
+  #   { name: "Get books from library", completed_at: "6:00 PM"},
+  #   { name: "Grocery shopping", completed_at: "7:00 PM"}
+  # ]
   def index
     @tasks = Task.all
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    # puts ">>>> ECP: In TasksController#create"
+    # puts params[:task].to_hash
+    #
+    # Task.create(params[:task])
+    Task.create(task_params)
+
+    redirect_to tasks_path
   end
 
   def show
@@ -23,7 +34,14 @@ class TasksController < ApplicationController
     @task = Task.find[id]
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
   # def random_time
   #   Time.at(rand * Time.now.to_i)
   # end
+  private
+  def task_params
+    return params.require(:task).permit(:name, :description, :completed_at)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   root 'welcome#index', as: 'welcome'
 
   get 'tasks', to: 'tasks#index', as: 'tasks'
-
   get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create'
+
 
   get 'tasks/:id', to: 'tasks#show', as: 'task'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit-task'
 
 end
