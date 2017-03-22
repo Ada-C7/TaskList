e4c5958a6cc9543400fe7fe7e94e3605ae608a74
laura <lmelgarejos@unal.edu.co>
2017-03-22 15:54:52 -0700

    Added a new action 'edit'

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,14 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def edit
+    task = Task.find(params[:id])
+    task(task_params)
+    task.save
+
+    redirect_to tasks_path(task.id)
+  end
+
   def create
     Task.create(task_params)
     redirect_to tasks_path
