f20deb8e28264f3482514522438e6ebff3ead6fa
Theresa Manney <theresa.m.17@gmail.com>
2017-03-21 21:24:21 -0700

    Added another task and some info about the task, made each task easier to understand with some html YAY

---
+++ db/migrate/20170322040153_add_tasks.rb
@@ -0,0 +1,5 @@
+class AddTasks < ActiveRecord::Migration[5.0]
+  def change
+    add_column :tasks, :due_date, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321211612) do
+ActiveRecord::Schema.define(version: 20170322040153) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170321211612) do
     t.string   "completion_date"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "due_date"
   end
 
 end
