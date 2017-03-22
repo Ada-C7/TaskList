ac81a5fb48979fa04b098c9d8a0e56f672b371fa
Janice Lichtman <jalichtman@gmail.com>
2017-03-22 15:47:01 -0700

    Used view helper form_for tag for the new task form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,27 +1,19 @@
 class TasksController < ApplicationController
 
-
   def index
     @tasks = Task.all
   end
 
-
   def new
+    @task=Task.new
   end
 
   def create
-    puts ">>>JCL: In BooksController#create"
-    puts params.to_hash
-
     Task.create(task_params)
-
     redirect_to tasks_path
-
   end
 
   def show
-    puts ">>>JCL: In BooksController#show"
-    puts params.to_hash
     id = params[:id].to_i
     @task = Task.find(id)
   end
@@ -30,6 +22,5 @@ class TasksController < ApplicationController
   def task_params
     return params.require(:task).permit(:name, :description, :completed_at, :done)
   end
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ get 'tasks', to:'tasks#index'
 get 'tasks/new', to:'tasks#new', as: 'new_task'
 post 'tasks', to:'tasks#create'
 
-
 get 'tasks/:id', to:'tasks#show', as:'task'
 
 end
