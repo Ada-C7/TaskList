94dfd26f3d89269514a5ac8ca4da3c6179f8bf9c
Lynn Trickey <lynn.trickey@gmail.com>
2017-03-22 13:06:59 -0700

    created and ran migration to change column data type to text for description in tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321212854) do
+ActiveRecord::Schema.define(version: 20170322200330) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
-    t.string   "description"
+    t.text     "description"
     t.date     "completion_date"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
