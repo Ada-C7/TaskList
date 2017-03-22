1e363874cf0abcb199d41ab0a123f1982a5b72f7
Emma Claire Philippides <acme9898@gmail.com>
2017-03-22 10:18:27 -0700

    new task page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,26 @@
 class TasksController < ApplicationController
-TASKS = [
-  { name: "Pack bags", completed_at: "1:00 PM" },
-  { name: "Print out reservation confirmations", completed_at: "1:30 PM" },
-  { name: "Check-in for flight", completed_at: "2:00 PM" },
-  { name: "Buy sunscreen", completed_at: "2:15 PM" },
-  { name: "Prepare things kids will need", completed_at: "3:00 PM" },
-  { name: "Pack the chargers", completed_at: "2:30 PM" },
-  { name: "Write important info on Fridge board", completed_at: "3:00 PM" },
-  { name: "Get cash", completed_at: "4:00 PM" },
-  { name: "Get books from library", completed_at: "6:00 PM"},
-  { name: "Grocery shopping", completed_at: "7:00 PM"}
-]
+# TASKS = [
+#   { name: "Pack bags", completed_at: "1:00 PM" },
+#   { name: "Print out reservation confirmations", completed_at: "1:30 PM" },
+#   { name: "Check-in for flight", completed_at: "2:00 PM" },
+#   { name: "Buy sunscreen", completed_at: "2:15 PM" },
+#   { name: "Prepare things kids will need", completed_at: "3:00 PM" },
+#   { name: "Pack the chargers", completed_at: "2:30 PM" },
+#   { name: "Write important info on Fridge board", completed_at: "3:00 PM" },
+#   { name: "Get cash", completed_at: "4:00 PM" },
+#   { name: "Get books from library", completed_at: "6:00 PM"},
+#   { name: "Grocery shopping", completed_at: "7:00 PM"}
+# ]
   def index
-    @tasks = TASKS
+    @tasks = Task.all
+  end
+
+  def new
   end
 
   def show
     id = params[:id].to_i
-    @task = TASKS.find[id]
+    @task = Task.find[id]
   end
 
   # def random_time
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   root 'welcome#index', as: 'welcome'
 
-  get 'tasks', to: 'tasks#index'
+  get 'tasks', to: 'tasks#index', as: 'tasks'
+
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
 
   get 'tasks/:id', to: 'tasks#show', as: 'task'
 
