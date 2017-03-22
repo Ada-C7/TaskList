719f05f0743de9e0616b1ac50c8cac647de2ee16
add2point71dots <alcland@gmail.com>
2017-03-22 16:12:27 -0700

    did some styling on the form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
 
-  get '/tasks/:id/edit', to: "books#edit", as: 'edit_book'
+  get '/tasks/:id/edit', to: "tasks#edit", as: 'edit_task'
 
   get 'tasks/new', to: 'tasks#new', as: 'new_task'
   post '/tasks', to: 'tasks#create'
