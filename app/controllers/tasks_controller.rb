15fc98e17d6e30426603fc26283fdb07f21643b7
cecomfort <cara.comfort10@gmail.com>
2017-03-22 15:41:47 -0700

    Modified new.html to implement the 'form_for' rails view helper

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,10 +17,23 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params) # create new book and add to database
+
+    redirect_to tasks_path # return to books page
   end
 
   def show
     id = params[:id].to_i
     @task = Task.find(id)
+  end
+
+  private
+
+  def task_params # strong params, prevent bad user input, only permit a task and a name and description to describe that task
+    return params.require(:task).permit(:name, :description)
   end
 end
