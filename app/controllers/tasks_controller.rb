b1e50cb2766f71b8c0c477e931e19599d172361b
AlisonZ <alisonkzerbe@gmail.com>
2017-03-22 12:49:22 -0700

    added form element to create new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,18 @@ class TasksController < ApplicationController
       @task = Task.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    task = Task.new
+    task.name = params[:name]
+    task.description = params[:description]
+    # task.date
+
+    if task.save
+        redirect_to tasks_path
+    end
+  end
+
 end
