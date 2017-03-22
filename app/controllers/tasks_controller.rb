c51548bf373bf41f2ad7eeb0486d8aed535c74fb
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-03-22 10:44:22 -0700

    fixed broken next and prev buttons to no longer be hard-coded

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,27 +9,9 @@ class TasksController < ApplicationController
   def show
 
     @result_task = Task.find(params[:id])
-    tasks = Task.all
 
-    ids = []
-    tasks.each do |task|
-      ids << task[:id].to_i
-    end
-
-    max_id = ids.max
-    min_id = ids.min
-
-    if (params[:id].to_i - 1) >= min_id
-      @prev_id = (params[:id].to_i - 1).to_s
-    else
-      @prev_id = params[:id]
-    end
-
-    if (params[:id].to_i + 1) <= max_id
-      @next_id = (params[:id].to_i + 1).to_s
-    else
-      @next_id = params[:id]
-    end
+    @next_id = Task.where("id > ?", @result_task.id).first
+    @prev_id = Task.where("id < ?", @result_task.id).last
   end
 
   def new
