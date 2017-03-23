c9edf991e82ed99d1ec7e8fbbacb3ce03f8ce80f
Lynn Trickey <lynn.trickey@gmail.com>
2017-03-23 12:50:46 -0700

    Added update functionality to save changes to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,20 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def update
+    task = Task.find(params[:id])
+
+    task.name = task_params[:name]
+    task.description = task_params[:description]
+    task.status = task_params[:status]
+    task.complete_by = task_params[:complete_by]
+
+    if task.save
+      redirect_to task_path
+    end
+
+  end
+
   def new
     @task = Task.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get '/tasks/:id', to: 'tasks#show', as: 'task'
 
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  put '/tasks/:id', to: 'tasks#update'
 end
