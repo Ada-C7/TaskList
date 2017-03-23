7cbee1a5989f580733698e2be4b036f62a39addc
Janice Lichtman <jalichtman@gmail.com>
2017-03-22 17:08:24 -0700

    added ability to edit tasks, and a redirect if an invalid task is searched for

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,10 +13,29 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+
   def show
-    id = params[:id].to_i
-    @task = Task.find(id)
+
+
+    if !Task.exists?(params[:id])
+      redirect_to tasks_path
+    else
+      @task=Task.find(params[:id])
+    end
   end
+
+  def edit
+    @task=Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update_attributes(task_params)
+    task.save
+
+    redirect_to task_path(task)
+  end
+
 
   private
   def task_params
