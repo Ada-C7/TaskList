6df3a3a7e8537315f8e8f63eeb67a0bc05830aa8
Theresa Manney <theresa.m.17@gmail.com>
2017-03-22 19:50:11 -0700

    Completed all requirements for wave 2, added some css elements to make the pages look better

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,22 @@ class TasksController < ApplicationController
   def show
     @result_task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = Task.create task_params
+
+    unless task.id == nil
+      redirect_to tasks_path
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :description, :due_date, :completion_date)
+  end
+
 end
