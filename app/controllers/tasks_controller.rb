803bdc6006f490af09b3899a26601b7cb682a154
Chantal Rollison <rollison.chantal@gmail.com>
2017-03-22 18:16:39 -0700

    both waves completed, need to add more info about each task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,5 +12,17 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    task = Task.create task_params
 
+    unless task.id == nil
+      redirect_to tasks_path
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get "/tasks/new", to: "tasks#new", as: "new_task"
 
+  post "/tasks", to: "tasks#create"
+
   get "/tasks/:id", to: "tasks#show", as: "task"
 
 end
