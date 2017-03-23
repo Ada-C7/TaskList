4e7bb2e0439b85af93e6359bca1dfc3d0da1457f
Spatterjaaay <aja.pursova@gmail.com>
2017-03-23 15:54:52 -0700

    added edit task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,10 @@ class TasksController < ApplicationController
     @result_task = Task.find(params[:id])
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
   def new
     @task = Task.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   get '/tasks', to: 'tasks#index'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/tasks/:id/edit", to: "tasks#edit", as: "edit_task"
 end
