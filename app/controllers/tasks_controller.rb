911f98266426f1aa762e96fab96340dae7879201
EricaJCase <ericajcase@gmail.com>
2017-03-24 10:11:34 -0700

    added delete task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,8 +11,15 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def destroy
+     task = Task.find(params[:id])
+     task.destroy
+
+     redirect_to tasks_path
+  end
+
   def edit
-    @task=Task.find[:id]
+    @task=Task.find(params[:id])
   end
 
   def new
@@ -20,23 +27,22 @@ class TasksController < ApplicationController
   end
 
   def show
-    # id = params[:id].to_i
     @task = Task.find(params[:id])
-
-
   end
 
-  def update
-    task = Task.find(params[:id])
-    task.update_attributes(book_params)
 
-    redirect_to task_path(task)
-  end
+def update
+  task = Task.find(params[:id])
+  task.update_attributes(task_params)
+  task.save
+
+  redirect_to task_path(task)
+end
 
 
 
 private
-  def task_params
-    return params.require(:task).permit(:chore, :completed, :name)
-  end
+def task_params
+  return params.require(:task).permit(:chore, :completed, :name)
+end
 end
