daa414a8e7887c9c4e7f0d8839bdd587e51cfb5f
Tehut Getahun <tehut.m.g@gmail.com>
2017-03-24 10:21:31 -0700

    updated edit and delete method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,57 @@
 class TasksController < ApplicationController
 
-
   def index
     @tasks = Task.all
   end
 
+
   def new
+    @task = Task.new
   end
+
+  def update
+    puts ">>> TG: : InTasksController#update"
+    task = Task.find(params[:id])
+    task.update_attributes(task_params)
+    task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+  id = params[:id].to_i
+  @task = Task.find(id)
+  end
+
+  def create
+    puts ">>> TG: : InTasksController#create"
+    puts params[:task].to_hash
+
+    Task.create(task_params)
+    redirect_to tasks_path
+  end
+
 
   def show
     id = params[:id].to_i
-    @task = Task.find(id)
+    @task = Task.find(id) or raise not_found
+    rescue ActiveRecord::RecordNotFound
+    flash[:notice] = "Wrong post it"
+    redirect_to :action => 'index'
+  end
 
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_path
+  end
+
+
+  private
+  def task_params
+    return params.require(:task).permit(:name, :deadline)
+  end
+
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
   end
 end
