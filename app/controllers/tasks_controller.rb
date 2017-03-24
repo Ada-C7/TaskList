705e056df43a7c20fd8fe310b715046fe427360e
Emma Claire Philippides <acme9898@gmail.com>
2017-03-24 09:07:40 -0700

    destroy and update methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,5 @@
 class TasksController < ApplicationController
-  # TASKS = [
-  #   { name: "Pack bags", completed_at: "1:00 PM" },
-  #   { name: "Print out reservation confirmations", completed_at: "1:30 PM" },
-  #   { name: "Check-in for flight", completed_at: "2:00 PM" },
-  #   { name: "Buy sunscreen", completed_at: "2:15 PM" },
-  #   { name: "Prepare things kids will need", completed_at: "3:00 PM" },
-  #   { name: "Pack the chargers", completed_at: "2:30 PM" },
-  #   { name: "Write important info on Fridge board", completed_at: "3:00 PM" },
-  #   { name: "Get cash", completed_at: "4:00 PM" },
-  #   { name: "Get books from library", completed_at: "6:00 PM"},
-  #   { name: "Grocery shopping", completed_at: "7:00 PM"}
-  # ]
+
   def index
     @tasks = Task.all
   end
@@ -20,10 +9,6 @@ class TasksController < ApplicationController
   end
 
   def create
-    # puts ">>>> ECP: In TasksController#create"
-    # puts params[:task].to_hash
-    #
-    # Task.create(params[:task])
     Task.create(task_params)
 
     redirect_to tasks_path
@@ -37,9 +22,22 @@ class TasksController < ApplicationController
   def edit
     @task = Task.find(params[:id])
   end
-  # def random_time
-  #   Time.at(rand * Time.now.to_i)
-  # end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to @task
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
   def task_params
     return params.require(:task).permit(:name, :description, :completed_at)
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
 
   get 'tasks/:id', to: 'tasks#show', as: 'task'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit-task'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
 
 end
