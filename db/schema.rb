147c3f2a8bdfbd65170efced1bae170c7a8f5145
Tehut Getahun <tehut.m.g@gmail.com>
2017-03-24 11:47:04 -0700

    added completed column to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321192034) do
+ActiveRecord::Schema.define(version: 20170324184134) do
 
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
     t.datetime "deadline"
+    t.boolean  "completed"
   end
 
 end
