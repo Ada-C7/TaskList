7465fe39e974cbdc7e0db4b98e4f923dbc3d2770
Lynn Trickey <lynn.trickey@gmail.com>
2017-03-24 12:58:11 -0700

    Added root 'tasks#index' and delete path in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'tasks#index'
 
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
   post '/tasks', to: 'tasks#create'
@@ -9,4 +10,6 @@ Rails.application.routes.draw do
 
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   put '/tasks/:id', to: 'tasks#update'
+
+  delete '/books/:id', to: 'books#destroy', as: 'delete_task'
 end
