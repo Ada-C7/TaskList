0df642fafd95aa12fade48a2a8a8dd022cae5786
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-03-23 19:03:02 -0700

    Wave 3 lookin' good - added ability to edit tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,14 +7,31 @@ class TasksController < ApplicationController
     @show_task = Task.find(params[:id])
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+
+    task.name = task_params[:name]
+    task.description = task_params[:description]
+    task.completion_date = task_params[:completion_date]
+    # task.complete = task_params[:complete]
+
+    if task.save
+      redirect_to task_path(task.id)
+    end
+  end
+
   def new
     @task = Task.new
   end
 
-  def create
+  def create # how can I make this so it is requires to have a name??
     task = Task.create task_params
 
-    unless task.id == nil
+    unless task.name == nil
       redirect_to tasks_path
     end
   end
@@ -22,6 +39,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:name, :description, :completion_date)
+    params.require(:task).permit(:name, :description, :completion_date, :complete)
   end
 end
