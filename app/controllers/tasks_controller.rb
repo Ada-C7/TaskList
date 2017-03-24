e7014c0ef8428dc18cec879d7c71869d4d913b6f
Spatterjaaay <aja.pursova@gmail.com>
2017-03-24 15:51:26 -0700

    improved edit task, added delete task, asking user for confirmation, added button to mark the task complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,14 +11,41 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def update
+    task = Task.find(params[:id])
+      if task.update(task_params)
+        redirect_to task_path
+      end
+  end
+
+  def completed
+    task = Task.find(params[:id])
+    if task.completed
+       task.completed = false
+    else
+      task.completion_date = Time.now
+      task.completed = true
+    end
+
+    if task.save
+      redirect_to task_path
+    end
+  end
+
+  def destroy
+    Task.destroy(params[:id])
+    redirect_to tasks_path
+  end
+
   def new
     @task = Task.new
   end
 
   def create
     task = Task.create task_params
-
-    redirect_to tasks_path
+    unless task.id == nil
+      redirect_to tasks_path
+    end
   end
 
   private
