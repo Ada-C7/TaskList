f3dd2b74c59addef532c8e79f8b47ed2657eab33
Regan Huff <regan.huff@gmail.com>
2017-03-24 12:47:29 -0700

    Revise partial for form and add PATCH route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   get "/tasks/:id", to: "tasks#show", as: "task"
 
   get "/tasks/:id/edit", to: "tasks#edit", as: "edit_task"
-  put "/tasks/:id", to: "tasks#update"
+  patch "/tasks/:id", to: "tasks#update"
 
 end
