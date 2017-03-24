7188f328e85b8be03c40bfbee03f874e8017f9a9
Maria McGrew <mcg.maria@gmail.com>
2017-03-24 14:46:19 -0700

    finished submitting a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,9 +7,18 @@ class TasksController < ApplicationController
       @task = Task.new
     end
 
+    def create
+      Task.new(task_params)
 
+      redirect_to tasks_path
+    end
 
     def show
       @task = Task.find(params[:id])
+    end
+
+  private
+    def task_params
+      return params.require(:task).permit(:name, :description, :completed_at)
     end
   end
