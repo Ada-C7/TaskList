22ea220c4dd75ea919cd0d93ef6040a0279e4dc0
Allison Northrop <agnorthrop@gmail.com>
2017-03-24 14:47:13 -0700

    edit task link and functionality working.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,15 +19,24 @@ class TasksController < ApplicationController
     task = Task.find(params[:id])
     task.update_attributes(task_params)
     task.save
+
     redirect_to task_path(task)
   end
 
   def create
     Task.create(task_params)
+
     redirect_to tasks_path
     # puts ">>>>> ANG: In TasksController#create"
     # puts params[:task].to_hash
     # Task.create(params[:task])
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+
+    redirect_to tasks_path
   end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,8 @@ Rails.application.routes.draw do
 
   get 'tasks/:id', to: 'tasks#show', as: 'task'
   get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  patch 'tasks/:id', to: 'task#update'
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
