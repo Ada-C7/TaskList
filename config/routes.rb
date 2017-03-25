21859feab39868e7faf02023c3d45d9a358f9d72
laura <lmelgarejos@unal.edu.co>
2017-03-25 11:04:55 -0700

    Finised project

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'tasks/:id', to: 'tasks#show', as: 'task'
   get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  post 'tasks/:id/edit', to: 'tasks#edit'
   patch 'tasks/:id', to: 'tasks#update'
   post 'tasks/:id/completed', to: 'tasks#completed', as: 'completed_task'
   delete 'tasks/:id', to: 'tasks#destroy'
