df9c9d825be607a087e7e44ea11f180e1f0f04d3
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-03-25 15:24:55 -0700

    This version is before generating migration to add a column.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,29 +1,21 @@
 class TasksController < ApplicationController
   def index
-    # @tasks = ["Buy Grocery", "Unload Dishwasher", "Call the Insurance"]
-    # @my_assignment = Assignment.all
     @my_task = Task.all
-
   end
 
   def show
     @result_task = Task.find(params[:id])
-    # @my_assignment = assignment.find(params[:id])
-
   end
 
-# Edit a Task
-
   def edit
-  @task = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def update
-
     task = Task.find(params[:id])
-     if task.update(task_params)
-       redirect_to tasks_path
-     end
+    if task.update(task_params)
+      redirect_to tasks_path
+    end
   end
 
   def completed
@@ -36,12 +28,8 @@ class TasksController < ApplicationController
     task.save
 
     redirect_to tasks_path
-
-
-
   end
 
-# Create a Form to add a new task
   def new
     @task = Task.new
   end
@@ -66,13 +54,11 @@ class TasksController < ApplicationController
     Task.destroy(params[:id])
 
     redirect_to tasks_path
-
   end
 
-    private
-
-    def task_params
-      params.require(:task).permit(:name, :description, :Due_date)
-    end
+  private
+  def task_params
+    params.require(:task).permit(:name, :description, :Due_date)
+  end
 
 end
