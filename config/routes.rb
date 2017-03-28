169ce4b2f32ed5406534d3d5b5d297851be0c3d0
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-03-28 13:20:51 -0700

    deleted some comments and ready to submit

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-
   # put routes that act on a collection first
+  root to: 'tasks#index'
   get 'tasks', to: 'tasks#index', as: 'tasks'
-
-  # routes that create or edit go in middle
   get 'tasks/new', to: 'tasks#new', as: 'new_task'
-
   post 'tasks', to: 'tasks#create'
 
-  # routes that act on one instance go here
+  # routes that act on one instance go last
   # the as task is a little bit of "rails magic" that sets up routes to each individual task
   get 'tasks/:id', to: 'tasks#show', as: 'task'
   get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
@@ -16,7 +13,6 @@ Rails.application.routes.draw do
   patch 'tasks/:id', to: 'tasks#update'
   patch 'tasks/:id/completed', to: 'tasks#completed', as: 'completed'
 
-  # trying to create destory/delete path
   delete 'tasks/:id', to: 'tasks#delete'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
